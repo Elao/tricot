bff7e0591e1abc89c8c6959bb8fc691b40420e00
Thomas Jarrand <thomas.jarrand@gmail.com>
2017-11-15 18:11:38 +0100

    Scarf machine is comming!

---
+++ src/pattern/constants.js
@@ -0,0 +1,39 @@
+/**
+ * Colors
+ *
+ * @type {String}
+ */
+export const COLOR_RED = '#f63f3a';
+export const COLOR_WHITE = '#ffffff';
+
+/**
+ * Point size
+ *
+ * @type {Number}
+ */
+export const WIDTH = 12;
+export const HEIGHT = 14;
+export const LINE = 60;
+
+/**
+ * Repeat a pattern to fit the width of the line
+ *
+ * @param {String} pattern
+ * @param {Number} line
+ *
+ * @return {String}
+ */
+export function repeat(pattern, line = LINE) {
+  return pattern.repeat(line / pattern.length);
+}
+
+/**
+ * Concatenate strings
+ *
+ * @param {...String} lines
+ *
+ * @return {String}
+ */
+export function join(...lines) {
+  return lines.join('');
+}

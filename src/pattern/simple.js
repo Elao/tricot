fbdea344356819d728ad21b67b329f7d41d3db38
Benjamin Lévêque <benjamin@leveque.me>
2017-11-16 10:23:29 +0100

    Reindeer pattern

--- src/pattern/simple.js
+++ src/pattern/simple.js
@@ -33,3 +33,23 @@ export const largeSin = join(
   repeat(' v v'),
   repeat('v   '),
 );
+
+/**
+ *
+ */
+export const reindeer = join(
+  repeat(' v    v v v     v v v    v '),
+  repeat('vvv   v v vv   vv v v   vvv'),
+  repeat('vvv    vv v     v vv    vvv'),
+  repeat('vv      vvvv   vvvv      vv'),
+  repeat('v       vvvvv vvvvv       v'),
+  repeat('        vv       vv        '),
+  repeat('        vv       vv        '),
+  repeat('    vvv vv vv vv vv vvv    '),
+  repeat(' v vvvvvvv vvvvv vvvvvvv v '),
+  repeat('  vvvvvvv   vvv   vvvvvvv  '),
+  repeat('  vvvvvvvv   v   vvvvvvvv  '), 
+  repeat('  v v  v vv     vv v  v v  '),
+  repeat('  v v  v  v     v  v  v v  '),
+  repeat('  v v  v           v v v   '),
+);

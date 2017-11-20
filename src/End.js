abbc43e1e6f56f715505bd7015d724ecef6a6f08
Amelie DEFRANCE <amelie.defrance@elao.com>
2017-11-20 17:25:40 +0100

    inte end screen

--- src/End.js
+++ src/End.js
@@ -7,13 +7,29 @@ export default class End extends Component {
 
     return (
       <div className="end-panel">
-        <h3>Bravo !</h3>
-        <dl className="stats">
-            <dd>Score :</dd><dt>{getSuccessRatio(answers, 100).toPrecision(2)}%</dt>
-            <dd>Plus longue série :</dd><dt>{getLongestStreak(answers)}</dt>
-        </dl>
-        <p>Un projet web à tricoter ? Faite appel à une équipe d'expert, <a href="mailto: contact@elao.com">contactez élao</a>.</p>
-        <p>Appuie sur <strong>↑</strong> pour rejouer.</p>
+        <div className="end-panel__title">
+          <img src="images/wool.svg" alt=""/>
+          <h3>Bravo !</h3>
+        </div>
+        <div className="end-panel__statistics">
+          <dl>
+            <dd>Score :</dd>
+            <dt>{getSuccessRatio(answers, 100).toPrecision(2)}%</dt>
+          </dl>
+          <dl>
+            <dd>Plus longue série :</dd>
+            <dt>{getLongestStreak(answers)}</dt>
+          </dl>
+        </div>
+        <p>Un projet web à tricoter ?</p>
+        <p>Faites appel à une équipe d'experts.</p>
+        <div className="end-panel__buttons">
+          <a href="mailto: contact@elao.com" className="button">Contactez élao</a>
+          <button className="button button--light">
+            <span className="arrow up"></span>
+            Rejouer
+          </button>
+        </div>
       </div>
     );
   }

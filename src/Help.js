8936efd9c84ea3be5424c6af47ba44a04a128e12
Amelie DEFRANCE <amelie.defrance@elao.com>
2017-11-27 17:37:31 +0100

    wip mobile

--- src/Help.js
+++ src/Help.js
@@ -4,11 +4,13 @@ export default class Help extends Component {
   render() {
     return (
       <div className="rules">
-        Appuyez sur
-        <span className="icon arrow left"></span>
-        <span className="icon arrow down"></span>
-        <span className="icon arrow right"></span>
-        pour commencer.
+        <span>
+          Appuyez sur
+          <span className="icon arrow left"></span>
+          <span className="icon arrow down"></span>
+          <span className="icon arrow right"></span>
+        </span>
+        <span>pour commencer.</span>
       </div>
     );
   }

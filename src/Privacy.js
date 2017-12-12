ecfad76aa0bf682f93673b0c467fc1e802d67afc
Thomas Jarrand <thomas.jarrand@gmail.com>
2017-12-12 10:11:31 +0100

    Fixed typo

--- src/Privacy.js
+++ src/Privacy.js
@@ -12,7 +12,7 @@ export default class Privacy extends Component {
         <h3>Cookies et protection des données</h3>
         <p>Nous traquons le nombre de visites sur Knittar Hero.</p>
 
-        <p>Afin d&apos;être conforme aux la recommandation de la CNIL en matière de cookies, les mesures de protection des données
+        <p>Afin d&apos;être conforme aux recommandations de la CNIL en matière de cookies, les mesures de protection des données
 personnelles ont été mises en place :</p>
         <ul>
           <li>Votre adresse IP est anonymisée.</li>
